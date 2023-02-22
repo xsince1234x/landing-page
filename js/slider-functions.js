41689407514c95c8b9365ebc2b25621ff0d9658b
howardamz <howard@amzingppc.com>
2023-02-22 12:32:06 -0400

    slider finished

---
+++ js/slider-functions.js
@@ -0,0 +1,33 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    loopFillGroupWithBlank: true,
+    pagination:{
+        el: ".swiper-pagination",
+        clickeable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+    autoplay: {
+        delay: 2000,
+    },
+});
