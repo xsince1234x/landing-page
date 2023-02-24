67110b2103bfe59b36ad4bf12ce7931b2ccf19c0
howardamz <howard@amzingppc.com>
2023-02-24 12:42:25 -0400

    practica2

--- PRACTICA/registrar_comentario.php
+++ PRACTICA/registrar_comentario.php
@@ -1,9 +1,11 @@
 <?PHP
-$email = "user@user.com";
-$comment = "hola soy un comentario";
+header("Content-Type: application/x-www-form-urlencoded; charset=UTF-8");
+$obj = json_decode($_POST["json"], false);
+
 $connect = new mysqli('localhost','root','','practica');
 
-$consulta = "INSERT INTO comentarios (email,text) VALUES ('$email','$comment')";
+$consulta = "INSERT INTO comentarios (email,text) VALUES ('$obj->email','$obj->comentario')";
 $connect->query ($consulta);
 
+echo "code: 200";
 ?>
