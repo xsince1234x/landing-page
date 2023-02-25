8ada6cf42f726c1cb9fec31b711fee0b72b0c02d
howardamz <howard@amzingppc.com>
2023-02-24 20:42:11 -0400

    practica 3

--- PRACTICA/cargar_comentarios.php
+++ PRACTICA/cargar_comentarios.php
@@ -3,8 +3,14 @@ $connect = new mysqli('localhost','root','','practica');
 $consulta = "SELECT email,text FROM comentarios";
 $request=$connect->query($consulta);
 
+$comentarios[] = array();
+$cont = 0;
+
 while ($array=$request->fetch_array()) {
-   $db=("email"=>$array['email']) ;
+   $comentarios[$cont]['email'] = $array['email'];
+   $comentarios[$cont]['text'] = $array['text'];
+   $cont++;
 }
- echo json_encode($db);
+
+ echo json_encode($comentarios);
 ?>
